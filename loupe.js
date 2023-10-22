f02d22f30f3e05d6183dfde50a89c924e671680e
Dmitry Egorov <info@consultapp.ru>
2023-10-22 22:29:31 +0300

    loupe fixes

--- loupe.js
+++ loupe.js
@@ -1,28 +1,48 @@
-window.addEventListener("load", () => {
-  const mainPhoto = document.querySelector(
-    "#innerSlider .photoSlider__mainPhoto"
-  );
-  const mainPhotoWrapper = document.querySelector(
-    "#innerSlider .photoSlider__mainPhotoWrapper"
-  );
+window.addEventListener('load', () => {
+  const photo = document.querySelector('#innerSlider .photoSlider__mainPhoto')
+  const loupe = document.querySelector('#innerSlider .loupe')
 
-  const mainCenterX = Math.round(mainPhotoWrapper.clientWidth / 2);
-  const mainCenterY = Math.round(mainPhotoWrapper.clientHeight / 2);
-  const maxTranslate = 100;
-  const stepX = mainCenterX / maxTranslate;
-  const stepY = mainCenterY / maxTranslate;
+  loupe.addEventListener('mouseenter', () => {
+    loupe.style.background = `url(${photo.src})`
+  })
 
-  mainPhotoWrapper.addEventListener("mouseout", (e) => {
-    mainPhoto.style.transform = `scale(1)`;
-    mainPhoto.style.transform += `translateX(0px) `;
-    mainPhoto.style.transform += `translateY(0px) `;
-  });
+  loupe.addEventListener('mousemove', (e) => {
+    const currentX =
+      100 - ((loupe.clientWidth - e.layerX) / loupe.clientWidth) * 100
+    const currentY =
+      100 - ((loupe.clientWidth - e.layerY) / loupe.clientWidth) * 100
+    loupe.style.backgroundPosition = `${currentX}% ${currentY}% `
+  })
 
-  mainPhotoWrapper.addEventListener("mousemove", (e) => {
-    const currentX = (mainCenterX - e.layerX) / stepX;
-    const currentY = (mainCenterY - e.layerY) / stepY;
-    mainPhoto.style.transform = `scale(2)`;
-    mainPhoto.style.transform += `translateX(${currentX}px) `;
-    mainPhoto.style.transform += `translateY(${currentY}px) `;
-  });
-});
+  loupe.addEventListener('mouseleave', (e) => {
+    loupe.style.background = ''
+  })
+})
+// window.addEventListener("load", () => {
+//   const mainPhoto = document.querySelector(
+//     "#innerSlider .photoSlider__mainPhoto"
+//   );
+//   const mainPhotoWrapper = document.querySelector(
+//     "#innerSlider .photoSlider__mainPhotoWrapper"
+//   );
+
+//   const mainCenterX = Math.round(mainPhotoWrapper.clientWidth / 2);
+//   const mainCenterY = Math.round(mainPhotoWrapper.clientHeight / 2);
+//   const maxTranslate = 100;
+//   const stepX = mainCenterX / maxTranslate;
+//   const stepY = mainCenterY / maxTranslate;
+
+//   mainPhotoWrapper.addEventListener("mouseout", (e) => {
+//     mainPhoto.style.transform = `scale(1)`;
+//     mainPhoto.style.transform += `translateX(0px) `;
+//     mainPhoto.style.transform += `translateY(0px) `;
+//   });
+
+//   mainPhotoWrapper.addEventListener("mousemove", (e) => {
+//     const currentX = (mainCenterX - e.layerX) / stepX;
+//     const currentY = (mainCenterY - e.layerY) / stepY;
+//     mainPhoto.style.transform = `scale(2)`;
+//     mainPhoto.style.transform += `translateX(${currentX}px) `;
+//     mainPhoto.style.transform += `translateY(${currentY}px) `;
+//   });
+// });
