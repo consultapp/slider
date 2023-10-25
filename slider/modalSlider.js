a521b089391423019ba400ed467dd0a7ea678411
Dmitry Egorov <info@consultapp.ru>
2023-10-25 10:33:44 +0300

    sticky menu fix

--- slider/modalSlider.js
+++ slider/modalSlider.js
@@ -3,13 +3,21 @@ window.addEventListener("load", () => {
   const openers = document.querySelectorAll("[data-modal-opener]");
 
   document.getElementById("closeModalBtn").addEventListener("click", () => {
+    const header1 = document.querySelector(".stickytop");
+    const header2 = document.querySelector(".stickytop-nav");
     modal.removeAttribute("data-open");
     document.body.removeAttribute("data-open");
+    if (header1) header1.style.display = "block";
+    if (header2) header2.style.display = "block";
   });
   openers.forEach((item) => {
     item.addEventListener("click", () => {
+      const header1 = document.querySelector(".stickytop");
+      const header2 = document.querySelector(".stickytop-nav");
       modal.setAttribute("data-open", "");
       document.body.setAttribute("data-open", "");
+      if (header1) header1.style.display = "none";
+      if (header2) header2.style.display = "none";
     });
   });
 });
