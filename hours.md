52c83dd38bf55354dcd0de94b70c75ac814d80ac
Dmitry Egorov <info@consultapp.ru>
2023-10-25 11:16:07 +0300

    loupe

--- hours.md
+++ hours.md
@@ -5,3 +5,7 @@ modal: 1.5h
 loupe: 1h
 fixes: 1h
 php+struct updates:1h
+vertical js+css: 1h
+fixes+test after changes: 0.5h
+
+total: 8.5h
--- slider/loupe.js
+++ slider/loupe.js
@@ -1,48 +1,20 @@
-window.addEventListener('load', () => {
-  const photo = document.querySelector('#innerSlider .photoSlider__mainPhoto')
-  const loupe = document.querySelector('#innerSlider .loupe')
+window.addEventListener("load", () => {
+  const photo = document.querySelector("#innerSlider .photoSlider__mainPhoto");
+  const loupe = document.querySelector("#innerSlider .loupe");
 
-  loupe.addEventListener('mouseenter', () => {
-    loupe.style.background = `url(${photo.src})`
-  })
+  loupe.addEventListener("mouseenter", () => {
+    loupe.style.background = `url(${photo.src})`;
+  });
 
-  loupe.addEventListener('mousemove', (e) => {
+  loupe.addEventListener("mousemove", (e) => {
     const currentX =
-      100 - ((loupe.clientWidth - e.layerX) / loupe.clientWidth) * 100
+      100 - ((loupe.clientWidth - e.layerX) / loupe.clientWidth) * 100;
     const currentY =
-      100 - ((loupe.clientWidth - e.layerY) / loupe.clientWidth) * 100
-    loupe.style.backgroundPosition = `${currentX}% ${currentY}% `
-  })
+      100 - ((loupe.clientWidth - e.layerY) / loupe.clientWidth) * 100;
+    loupe.style.backgroundPosition = `${currentX}% ${currentY}% `;
+  });
 
-  loupe.addEventListener('mouseleave', (e) => {
-    loupe.style.background = ''
-  })
-})
-// window.addEventListener("load", () => {
-//   const mainPhoto = document.querySelector(
-//     "#innerSlider .photoSlider__mainPhoto"
-//   );
-//   const mainPhotoWrapper = document.querySelector(
-//     "#innerSlider .photoSlider__mainPhotoWrapper"
-//   );
-
-//   const mainCenterX = Math.round(mainPhotoWrapper.clientWidth / 2);
-//   const mainCenterY = Math.round(mainPhotoWrapper.clientHeight / 2);
-//   const maxTranslate = 100;
-//   const stepX = mainCenterX / maxTranslate;
-//   const stepY = mainCenterY / maxTranslate;
-
-//   mainPhotoWrapper.addEventListener("mouseout", (e) => {
-//     mainPhoto.style.transform = `scale(1)`;
-//     mainPhoto.style.transform += `translateX(0px) `;
-//     mainPhoto.style.transform += `translateY(0px) `;
-//   });
-
-//   mainPhotoWrapper.addEventListener("mousemove", (e) => {
-//     const currentX = (mainCenterX - e.layerX) / stepX;
-//     const currentY = (mainCenterY - e.layerY) / stepY;
-//     mainPhoto.style.transform = `scale(2)`;
-//     mainPhoto.style.transform += `translateX(${currentX}px) `;
-//     mainPhoto.style.transform += `translateY(${currentY}px) `;
-//   });
-// });
+  loupe.addEventListener("mouseleave", (e) => {
+    loupe.style.background = "";
+  });
+});
